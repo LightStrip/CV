b897a60395596935777c0d3cafe95ce1063b640c
LightStrip <96375474+LightStrip@users.noreply.github.com>
2023-06-01 12:18:08 +0300

    tetris in c#

---
+++ tetris/WpfApp2/WpfApp2/AI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Tetris
+{
+    public class AI
+    {
+        public GameState gameState { get; }
+
+        GameState state = new GameState();
+
+        public int x = 1;
+        public void use(bool yes)
+        {
+            yes = false; 
+                if(yes == true)
+            { 
+                state.RotateBlockCW();
+             }
+            
+        }
+    }
+}
---
+++ tetris/WpfApp2/WpfApp2/Jblocks.cs
@@ -0,0 +1,20 @@
+﻿namespace Tetris
+{
+    public class JBlock : Block
+    {
+        public override int Id => 2;
+
+        protected override Position StartOffset => new (0, 3);
+
+        protected override Position[][] Tiles => new Position[][]
+            {
+            new Position[] { new(0, 0), new(0, 2), new(1, 1), new(2, 1)},//mirrored upside down
+            new Position[] { new(0, 1), new(0, 2), new(1, 1), new(2, 1)},//mirrored upside down
+            
+            new Position[] { new(1, 0), new(1, 1), new(1, 2), new(2, 2)},//mirrored j
+            new Position[] { new(0, 1), new(1, 1), new(2, 1), new(2, 0)}
+        };
+
+    }
+}
+
